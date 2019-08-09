1f698a13a693b5387a0db069ebc28c7495cc5137
luisGuarda <luis.guarda.fernandez@outlook.com>
2019-08-09 17:16:23 -0500

    chat terminado

--- js/post.js
+++ js/post.js
@@ -152,66 +152,12 @@ $(document).ready(function() {
         var textoCLI = mensajellenar;
         var hora = new Date().toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit' });
 
-        var divCLI = $("#template").clone();
-        document.getElementById("cargando").style.display = 'block';
+        $('#horabienvenida').text(hora);
 
+        console.log(hora);
 
-        $.ajax({
-            type: 'POST',
-            url: "https://api.us-south.apiconnect.appdomain.cloud/caja-arequipa-apic-dev/desarrollo/chatbot/message",
-            headers: { "Content-Type": "application/json", "x-ibm-client-id": "5211d0a0-594b-404f-af6c-bb154abcc631" },
-            dataType: "json",
-            data: '{"clientId":"' + idrandom + '","mensaje":"hola","plataforma":"web"}',
-            success: function(data) {
-
-                console.log("codigo aleatorio: " + idrandom);
-                var respuestaCA = data.text || '';
-                var respuestaCATitulo = data.tituloOptions || '';
-                var respuestaCAOpcion = data.options || [];
-
-                var elMsgTemplate = $("#template").clone();
-                elMsgTemplate.removeAttr('id').removeClass('d-none');
-                elMsgTemplate.find('.divrespuestacli').addClass('d-none');
-                elMsgTemplate.find('.horaenvio').addClass('d-none');
-                elMsgTemplate.find('.msgenvio').addClass('d-none');
-                var horaRecibido = new Date().toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit' });
-                elMsgTemplate.find('.horabot').html(horaRecibido);
-
-                if (respuestaCA !== undefined && respuestaCATitulo !== undefined && respuestaCAOpcion !== undefined) {
-                    elMsgTemplate.find('.respuestaCA').html(respuestaCA);
-                    elMsgTemplate.find('.respuestaCATitulo').html(respuestaCATitulo);
-
-                    if (respuestaCAOpcion.length > 0) {
-                        elMsgTemplate.find('.textoseleccione').removeClass('d-none').html('Seleccione una de estas opciones');
-                        $.each(respuestaCAOpcion, function(index, item) {
-                            createButton(elMsgTemplate, item);
-                        });
-                    }
-                } else if (respuestaCA) {
-                    elMsgTemplate.find('.respuestaCA').addClass('d-none');
-                    elMsgTemplate.find('.respuestaCATitulo').html(respuestaCA);
-                } else {
-                    elMsgTemplate.find('.respuestaCA').addClass('d-none');
-                    elMsgTemplate.find('.respuestaCATitulo').html(respuestaCATitulo);
-                    $.each(respuestaCAOpcion, function(index, item) {
-                        createButton(elMsgTemplate, item);
-                    });
-                }
-
-
-                document.getElementById("cargando").style.display = 'none';
-                $("#respuestasbot").append(elMsgTemplate);
-                var posicion = $("#scrollfocus").offset().top;
-                $(".card-body.comments-main").animate({
-                    scrollTop: $(".p-0").height()
-                }, 2000);
-
-
-
-            }
-
-        });
     });
+
 
 
     function Opciones(e) {
